145d689b34607f82859144f92cdd244af74886b7
Sebastian Pratto <pratto.sebastian.ignacio@gmail.com>
2021-09-14 13:39:11 -0300

    Read values from preferences

--- app/src/main/java/com/codesol/apppreferences/MainActivity.java
+++ app/src/main/java/com/codesol/apppreferences/MainActivity.java
@@ -2,13 +2,25 @@ package com.codesol.apppreferences;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etValue = findViewById(R.id.etPrefer);
+        SharedPreferences preferencias = getSharedPreferences("data", Context.MODE_PRIVATE);
+        etValue.setText(preferencias.getString("value",""));
     }
+
+
+
 }
